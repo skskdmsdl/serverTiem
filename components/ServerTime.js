8065a43907179513a43d4579e52c8c80dafdfe5b
skskdmsdl <skskdmsdl@gmail.com>
2023-05-24 23:13:35 +0900

    serverTime component 작성

---
+++ components/ServerTime.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+const ServerTime = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <View>
+      <Text>현재 시간: {currentTime.toLocaleString()}</Text>
+    </View>
+  );
+};
