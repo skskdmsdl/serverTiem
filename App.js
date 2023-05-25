496088bfb1e7efb31a61b82d333264990790f85d
skskdmsdl <skskdmsdl@gmail.com>
2023-05-25 22:17:26 +0900

    serverTime 컴포넌트 위치 변경 및 전체 폴더 및 파일 경로 다시 설정

--- App.js
+++ App.js
@@ -1,22 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './src/App';
 
-import ServerTime from 'components/ServerTime';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <ServerTime />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
