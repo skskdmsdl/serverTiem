165d42d9d40dac1b277802f397101b62b917d2fc
skskdmsdl <skskdmsdl@gmail.com>
2023-06-01 21:51:40 +0900

    notification 적용 중

--- src/components/ServerTime.js
+++ src/components/ServerTime.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRefgi } from 'react';
 import { View, Text } from 'react-native';
 import * as Notifications from 'expo-notifications';
 
@@ -12,6 +12,10 @@ Notifications.setNotificationHandler({
 
 const ServerTime = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
   useEffect(() => {
     const timer = setInterval(() => {
